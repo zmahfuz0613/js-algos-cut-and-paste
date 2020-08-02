5621674ac6e7ca9284dcc14b5d5243a878898ffc
zmahfuz061318 <str053mgr@gmail.com>
2020-08-01 22:51:58 -0400

    Completed

--- cutAndPaste.js
+++ cutAndPaste.js
@@ -2,11 +2,14 @@
 
 const cutAndPaste = (arr, num) => {
 
+  let numbers = arr.slice(0, num);
+  arr.splice(0, num);        
+  arr.push(...numbers);
+  return arr;
 }
 
 console.log(cutAndPaste([1, 2, 5, "help", "wow", 3], 1))
 
 module.exports = {
-  cutAndPaste
+cutAndPaste
 };
-// 
